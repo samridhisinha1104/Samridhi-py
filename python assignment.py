7c6a704c2e690006b2fa3ca81bb61865743ce0e3
samridhisinha1104 <167615423+samridhisinha1104@users.noreply.github.com>
2024-04-20 19:09:16 +0530

    Add files via upload

---
+++ python assignment.py
@@ -0,0 +1,21 @@
+Val = int(input("Value:"))  
+Adder = 0
+for C in range(1, Val, 3):  
+    if C % 2 == 0:  
+        print(C * 10)  
+    else:  
+        print(Adder) 
+print(C)  
+print(Adder)  
+
+
+Val = 25  
+for I in range(0, Val):  
+    if I % 2 == 0:  
+        print(I + 1)
+    else:  
+        print(I - 1)
+        
+STRING = "WELCOME NOTE"  
+for S in range(0, 8):  
+    print(STRING[S]) 
